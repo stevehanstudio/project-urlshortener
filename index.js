ca176918cae89621f8a25d52374878e805762f4e
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-07-05 16:31:40 -0700

    bug fixed

--- index.js
+++ index.js
@@ -2,11 +2,73 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+// const dns = require('node:dns')
+const dns = require('dns')
+const { parse: urlParser } = require('url')
+const mongoose = require('mongoose')
+const { count } = require('node:console')
+
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => {
+		console.log('Database connection successful')
+	})
+	.catch(err => {
+		console.error('Database connection error')
+	})
+
+const shortUrlSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: true
+  },
+})
+
+ShortUrl = mongoose.model('ShortUrl', shortUrlSchema)
+
+const createAndSaveUrl = async (url) => {
+  try {
+    const shortUrl = await ShortUrl.findOne({ url: url })
+    if (shortUrl) return shortUrl
+    const count = await ShortUrl.countDocuments({})
+    // console.log("Count:", count)
+    const newShortUrl = new ShortUrl({
+      id: count+1, url: url
+    })
+    const retUrl = await newShortUrl.save()
+    return retUrl
+  } catch (err) {
+    console.error("Error in createAndSaveUrl:", err)
+  }
+}
+
+const getUrlById = async (id) => {
+  try {
+    const shortUrl = await ShortUrl.find({ id: id })
+    return shortUrl
+  }
+  catch (err) {
+    return console.error(err)
+  }
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,8 +79,58 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
-});
+})
+
+app.post('/api/shorturl/', (req, res) => {
+  const { url: rawUrl } = req.body
+  const parsedUrl = urlParser(rawUrl)
+  const url = parsedUrl.protocol ? parsedUrl.host : parsedUrl.path
+  // console.log("About to DNS lookup url:", url);
+  dns.lookup(url,
+    (err, address, family) => {
+    if (err) {
+      // console.log('err:', err)
+      // console.log('address:', address)
+      // console.log('family:', family)
+      res.json({
+        "error": "invalid url"
+      })
+    } else {
+      createAndSaveUrl(url)
+        .then(shortUrl => {
+          // console.log("Returning from createAndSave with", shortUrl)
+          res.json({
+            "original_url": shortUrl.url,
+            "short_url": shortUrl.id
+          })
+        })
+        .catch(err => {
+          res.json({
+            error: 'Invalid URL',
+          })
+        })
+    }})
+})
+
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.json({
+      error: 'Invalid Url'
+    })
+  }
+  getUrlById(+id)
+    .then(shortUrl => {
+      console.log('shortUrl', shortUrl)
+      res.redirect(`https://${shortUrl[0].url}`)
+    })
+    .catch(err => {
+      console.log("URL not found in database", err)
+    })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
-});
+})
